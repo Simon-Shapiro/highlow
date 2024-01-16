d05877c7a186636daeb61de0afe8eb165c523d61
Simon <simonshapiro28@gmail.com>
2024-01-16 01:11:04 -0600

    adding debug files to remote repo; first draft of program.cs

--- Program.cs
+++ Program.cs
@@ -1,2 +1,46 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*This is a simple high/low game in C#. 
+1. Generate two numbers
+2. Display the first number
+3. Get the user's guess of whether the second number is higher or lower than the first.
+4. Display the second number
+5. Ask the user if they would like to play again
+*/
+
+Random number = new Random();
+
+//the second number must not be equal to the first number. Add logic loop such that if they are equal, new values are assigned
+int firstNumber = number.Next(1, 15);
+int secondNumber = number.Next(1, 15);
+
+Console.WriteLine($"The first number is {firstNumber}. Is the second number higher (A) or lower (B) than {firstNumber}?");
+
+//get user input -- .toLowerCase/ .toUpperCase on A/B to make input case insensitive
+
+do
+{
+    if (/*user input was A/a*/)
+    {
+        if (secondNumber > firstNumber)
+        {
+            Console.WriteLine($"You got it, the second number was higher!");
+        }
+        else
+        {
+            Console.WriteLine($"Too bad, the second number was lower :C");
+        }
+    }
+
+    if (/*user input was B/b*/)
+    {
+        if (secondNumber < firstNumber)
+        {
+            Console.WriteLine($"You got it, the second number was lower!");
+        }
+        else
+        {
+            Console.WriteLine($"Too bad, the second number was higher :C");
+
+        }
+    }
+
+} while (/*user has not hit esc to end the game*/);
