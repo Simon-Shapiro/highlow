986ed484c671f889f5ccc970829737b05a6c16d4
Simon <simonshapiro28@gmail.com>
2024-01-25 22:47:34 -0600

    moving escape keypress detection function to end of program.cs

--- obj/Debug/net8.0/highlow.AssemblyInfo.cs
+++ obj/Debug/net8.0/highlow.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("highlow")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3e0313d293fb18921c74751e9a641f8953babfa2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+299e365714d39d6af9fddfddeb92ed2e71ec9a01")]
 [assembly: System.Reflection.AssemblyProductAttribute("highlow")]
 [assembly: System.Reflection.AssemblyTitleAttribute("highlow")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
